4c3a69e3315b69f42d1f09ea43d32058c63ab392
Himmu-00007 <himmu00007@gmail.com>
2026-05-27 20:41:20 +0545

    Minor Corrections(1)

--- Firsttime.py
+++ Firsttime.py
@@ -1,9 +1,10 @@
-print ("Please pay attention!")while True:
+print ("Please pay attention!")
+while True:
     password = input("Enter password: ")
     if password == ("Gori@16#2066*"):
         print ("Unlocking...please wait!")
         print ("Bro, I thought you missed the password for the 67th time. Congrats bro, now you're the real minder!")
-        verify= input("Crush ko id chahiyo vane aafno birth date hala bro, I just want to verify if you are Himmu or not, USE '-' SIGN FOR SEPARATION: ")
+        verify= input("Crush ko id chahiyo vane usko birth date hala bro, I just want to verify if you are Himmu or not, Use '-' Sign for separation⚠️: ")
         if verify==("2066-08-13"):
             print ("oh, so u r Himmu. I'll tell you your crush's id in college. See you soon!,<3")
             break
